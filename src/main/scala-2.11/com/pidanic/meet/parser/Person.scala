96002033a6bce0f2e82296dd998e5ae9e9ddd63f
Paul P <paulp@hotmail.sk>
2016-02-01 10:50:35 +0100

    added checking of correct values and new string method

--- src/main/scala-2.11/com/pidanic/meet/parser/Person.scala
+++ src/main/scala-2.11/com/pidanic/meet/parser/Person.scala
@@ -3,4 +3,23 @@ package com.pidanic.meet.parser
 private[parser] sealed case class Person
   (resource: String, name: String, surname: String,
    givenName: String, description: String,
-   birthDate: String, deathDate: String)
+   birthDate: String, deathDate: String) {
+
+  require(resource != null)
+  require(name != null)
+  require(surname != null)
+  require(givenName != null)
+  require(description != null)
+  require(birthDate != null)
+  require(deathDate != null)
+
+  def toCsvString(separator: Char): String = {
+    resource + "" + separator + "" +
+      name + "" + separator + "" +
+      surname + "" + separator + "" +
+      givenName + "" + separator + "" +
+      description + "" + separator + "" +
+      birthDate + "" + separator + "" +
+      deathDate
+  }
+}
